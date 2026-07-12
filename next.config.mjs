b1b38bd2e6ba600d7e05b79fff5bc7fd41f31f78
RoxyEmanuel26 <bimantara1997@yahoo.com>
2026-07-13 05:28:41 +0700

    fix: add security headers config for Cloudflare Pages/Workers in next.config.mjs and public/_headers

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,31 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self';"
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
